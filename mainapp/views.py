bda9d69efee0c51479948c03d607977000efc496
JeronimoC23 <jeronimoclinaz@gmail.com>
2021-07-02 14:41:15 -0300

    Mejoras + redes

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from .forms import RegisterForm
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+
 
 
 # Create your views here.
@@ -17,47 +14,3 @@ def aboutus(request):
         'title':'About'
     })
 
-def register_page(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        register_form = RegisterForm()
-
-        if request.method == "POST":
-                register_form = RegisterForm(request.POST)
-
-                if register_form.is_valid():
-                    register_form.save()
-                    messages.success(request,'Success register!')
-                    return redirect('home')
-
-
-        return render(request, 'users/register.html',{
-            'title':'Sign in',
-            'register_form':register_form
-        } )
-
-def login_page(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        if request.method == "POST":
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-
-            user = authenticate(request, username=username, password=password)
-
-            if user == None:
-                messages.warning(request, "Can't login")
-                
-            else:
-                login(request, user)
-                return redirect('home')
-        
-        return render(request, 'users/login.html',{
-            'title':'Login'
-        })
-
-def logout_user(request):
-    logout(request)
-    return redirect('login')
