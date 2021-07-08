423f3501ea2d98ead9d87a3b7fd4fa02b6f821a7
JeronimoC23 <jeronimoclinaz@gmail.com>
2021-07-08 15:46:40 -0300

    + requirementes

--- DjangoWebProyect/settings.py
+++ DjangoWebProyect/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,4 +130,14 @@ STATIC_URL = '/static/'
 #media
 
 MEDIA_URL ="/media/"
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
