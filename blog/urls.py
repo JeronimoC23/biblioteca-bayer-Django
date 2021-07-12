b89959924cc86392b49804b5246aa7b9f6c1b526
JeronimoC23 <jeronimoclinaz@gmail.com>
2021-07-12 14:07:20 -0300

    add event detail + url

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('articles/', views.list, name="list"),
     path('article/<int:article_id>', views.article, name="article"),
+    path('event/<int:event_id>', views.event_detail, name="event"),
 ]
