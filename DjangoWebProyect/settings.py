1c71bb3f5c121babf8843617163b3086469f9604
MJoaaquin <joaquinmansilla@fiqus.com>
2021-08-30 14:59:09 -0300

    agregando db host

--- DjangoWebProyect/settings.py
+++ DjangoWebProyect/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
         'USER':os.getenv("DATABASE_USER"),
         'PASSWORD':os.getenv("DATABASE_PASSWORD"),
         'PORT':'25060',
+        'HOST': os.getenv("DATABASE_HOST")
     }
 }
 
