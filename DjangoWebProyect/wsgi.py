a198e9720d23a6d83c0e27438fcaeb769f0966dc
MJoaaquin <joaquinmansilla@fiqus.com>
2021-09-07 18:34:24 -0300

    agregar whitenoise a wsgi

--- DjangoWebProyect/wsgi.py
+++ DjangoWebProyect/wsgi.py
@@ -1,16 +1,6 @@
-"""
-WSGI config for DjangoWebProyect project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
-import os
-
+#wsgi.py
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoWebProyect.settings')
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
