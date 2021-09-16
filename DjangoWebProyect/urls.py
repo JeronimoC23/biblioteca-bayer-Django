9d2bf90411e259c1f0d9f9fa49ec2200a7b43de5
JeronimoC23 <jeronimoclinaz@gmail.com>
2021-09-16 17:21:44 -0300

    Ver media

--- DjangoWebProyect/urls.py
+++ DjangoWebProyect/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
 ]
 
 #Ruta imagenes
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
